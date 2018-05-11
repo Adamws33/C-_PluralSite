71b328ac6e8748bf3ae61c1aa8f3372961421ed0
Adam Smith <adamws33@gmail.com>
2018-05-11 13:39:54 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simple_Class Adam = new Simple_Class(34, "July");
+            Simple_Class Lindsey = new Simple_Class(31, "June");
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Adam.Age.GetHashCode());
+            Console.WriteLine(Lindsey.Age.GetHashCode());
+            Console.WriteLine(Adam.BirthMonth.GetHashCode());
+            Console.WriteLine(Lindsey.BirthMonth.GetHashCode());
+        }
+
+
+        
+
+    }
+}
---
+++ Simple_Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Simple_Class
+    {
+        public Simple_Class(int Age, string BirthMonth)
+        {
+            this.Age = Age;
+            this.BirthMonth = BirthMonth;
+        }
+        public int Age { get; set; }
+        public string BirthMonth { get; set; }
+    }
+}
